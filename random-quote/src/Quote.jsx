fae127c3bd5a0a06e5a1ca5811e336c281f93e63
kalkidan-solomon-coder <152845891+kalkidan-solomon-coder@users.noreply.github.com>
2024-04-16 18:10:37 +0300

    changes

---
+++ random-quote/src/Quote.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Quote(){
+const[quote,setQuote] = useState("")
+
+function generateQuote(){
+axios.get("http://api.quotable.io/random").then((res)=>{
+    setQuote(res.data.content)
+})
+}
+
+return(
+
+    <>
+    <div className="container">
+        <p className="heading">Random Quote Generator</p>
+    <p className="paragraph">{quote}</p>
+   <button onClick={generateQuote} className="button">Click</button>
+    </div>
+
+    </>
+)
+}
+export default Quote;
